6b568327fd0f31dcb0da7f007c20c3adc8fe72e7
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-03-09 14:31:45 +0530

    Update Positions of Large Groups.cpp

--- leetcode/String/concepts/Positions of Large Groups.cpp
+++ leetcode/String/concepts/Positions of Large Groups.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     vector<vector<int>> largeGroupPositions(string s) {
         vector<vector<int>> ans;
-        // i and j will start from the 0th index. but i will increment as i = j as jth value. here i will store the first occurence and j will store the last occurance.
-        // if s[i] and s[j] is equal then j++
-        // means consicutive charecters
-        // and lastly we will check if j - i is >= 3
-        // if is not then set i = j
-        for(int i = 0, j = 0; i< s.size(); i = j){
-            while(j < s.size() && s[i] == s[j]) j++;
-            if(j - i >= 3){
-                ans.push_back({i, j-1});
+        int n = s.size();
+        int start = 0, count = 1;
+
+        for(int i = 1; i< n; i++){
+            if(s[i] == s[i-1]){
+                count++;
+            }
+            else{
+                if(count >= 3){
+                    ans.push_back({start, i-1});
+                }
+                start = i;
+                count = 1;
             }
         }
+
+        if(count >= 3){
+            ans.push_back({start, n-1});
+        }        
+
         return ans;
     }
 };
