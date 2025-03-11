cffe07836fd9e54d9e10e88e30a6ee386826a49c
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-03-11 12:13:46 +0530

    Create Long Pressed Name.cpp

---
+++ leetcode/String/concepts/Long Pressed Name.cpp
@@ -0,0 +1,31 @@
+// Long Pressed Name
+// Example 1:
+
+// Input: name = "alex", typed = "aaleex"
+// Output: true
+// Explanation: 'a' and 'e' in 'alex' were long pressed.
+// Example 2:
+
+// Input: name = "saeed", typed = "ssaaedd"
+// Output: false
+// Explanation: 'e' must have been pressed twice, but it was not in the typed output.
+
+
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int i = 0, j = 0;
+
+        while (j < typed.size()) {
+            if (i < name.size() && name[i] == typed[j]) {
+                i++;
+                j++;
+            } else if (j > 0 && typed[j] == typed[j - 1]) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.size();
+    }
+};
