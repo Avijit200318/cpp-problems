b1ce547577cfadbbaa7df2dfc947ba4b5c1459dc
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-03-25 12:32:26 +0530

    Update Print all subsequence of sum K.cpp

--- Backtracking/Print all subsequence of sum K.cpp
+++ Backtracking/Print all subsequence of sum K.cpp
@@ -17,6 +17,27 @@ void subSequence(vector<int> &arr, int idx, int n, int target, int sum, vector<v
     subSequence(arr, idx+1, n, target, sum, ans, current);
 }
 
+// if we want to pring only one solution
+
+// bool solve(int idx, int n, int sum, vector<int> &arr, vector<vector<int>> &ans, vector<int> &current, int target){
+//     if(idx == n){
+//         if(sum == target){
+//             ans.push_back(current);
+//             return true;
+//         }
+//         return false;
+//     }
+
+//     current.push_back(arr[idx]);
+//     if(solve(idx+1, n, sum + arr[idx], arr, ans, current, target)) return true;
+
+//     current.pop_back();
+//     if(solve(idx+1, n, sum, arr, ans, current, target)) return true;
+
+//     return false;
+// }
+
+
 int main(){
     vector<int> arr = {1, 2, 1};
 
