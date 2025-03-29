ae6b091203df07c6d19a22288a1e3b81b3742a2d
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-03-29 11:20:41 +0530

    Update Frog Jump.cpp

--- Dinamic Programming/striver DP/Frog Jump.cpp
+++ Dinamic Programming/striver DP/Frog Jump.cpp
@@ -59,3 +59,25 @@ int frogJump(int n, vector<int> &heights)
 }
 
 
+// *****more optimize version*****
+#include <bits/stdc++.h> 
+
+int frogJump(int n, vector<int> &heights)
+{
+    if(n == 0) return 0;
+
+    int prev = 0, prev2, curr;
+
+    for(int i = 1; i< n; i++){
+        int left = prev + abs(heights[i] - heights[i-1]);
+        int right = INT_MAX;
+
+        if(i > 1){
+            right = prev2 + abs(heights[i] - heights[i-2]);
+        }
+        curr = min(left, right);
+        prev2 = prev;
+        prev = curr;
+    }
+    return curr;
+}
