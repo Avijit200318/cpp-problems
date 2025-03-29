389d1c816999ae54cd0fedfe96cd5516981943e6
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-03-29 11:02:21 +0530

    Create  Frog Jump.cpp

---
+++ Dinamic Programming/striver DP/Frog Jump.cpp
@@ -0,0 +1,33 @@
+// Problem statement
+// There is a frog on the '1st' step of an 'N' stairs long staircase. The frog wants to reach the 'Nth' stair. 'HEIGHT[i]' is the height of the '(i+1)th' stair.
+// If Frog jumps from 'ith' to 'jth' stair, the energy lost in the jump is given by absolute value of ( HEIGHT[i-1] - HEIGHT[j-1] ). If the Frog is on 'ith' staircase,
+// he can jump either to '(i+1)th' stair or to '(i+2)th' stair.
+// Your task is to find the minimum total energy used by the frog to reach from '1st' stair to 'Nth' stair.
+
+// For Example
+// If the given ‘HEIGHT’ array is [10,20,30,10], 
+// the answer 20 as the frog can jump from 1st stair to 2nd stair (|20-10| = 10 energy lost) and then a jump from 2nd stair to
+// last stair (|10-20| = 10 energy lost). So, the total energy lost is 20.
+
+#include <bits/stdc++.h> 
+
+int solve(int idx , vector<int> &memo, vector<int> &heights){
+    if(idx == 0){
+        return 0;
+    }
+    if(memo[idx] != -1) return memo[idx];
+
+    int left = solve(idx-1, memo, heights) + abs(heights[idx] - heights[idx-1]);
+    int right = INT_MAX;
+    if(idx > 1){
+        right = solve(idx - 2, memo, heights) + abs(heights[idx] - heights[idx-2]);
+    }
+    memo[idx] = min(left, right);
+    return memo[idx];
+}
+
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> memo(n+1, -1);
+    return solve(n-1, memo, heights);
+}
