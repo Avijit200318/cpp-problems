d00c0667446d197be271ea3c2b24b05a312f1e64
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-03-31 11:18:31 +0530

    Update Ninja’s Training.cpp

--- Dinamic Programming/striver DP/Ninja’s Training.cpp
+++ Dinamic Programming/striver DP/Ninja’s Training.cpp
@@ -110,3 +110,33 @@ int ninjaTraining(int n, vector<vector<int>> &points)
 
     return dp[n-1][3];
 }
+
+// *****more space optimize*******
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<int> prev(4, 0);
+
+    prev[0] = max(points[0][1], points[0][2]);
+    prev[1] = max(points[0][0], points[0][2]);
+    prev[2] = max(points[0][0], points[0][1]);
+    prev[3] = max(points[0][0], max(points[0][1] ,points[0][2]));
+
+    for(int day = 1; day< n; day++){
+        vector<int> temp(4, 0);
+        for(int last = 0; last < 4; last++){
+            int maxi = 0;
+
+            for(int i = 0; i< 3; i++){
+                if(i != last){
+                    int point = points[day][i] + prev[i];
+                    maxi = max(maxi, point);
+                }
+            }
+            temp[last] = maxi;
+        }
+        prev = temp;
+    }
+
+    return prev[3];
+}
+
