0c2fe9c8578ef4451eafc0d6d06fc75dc35052bd
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-08-30 19:34:09 +0530

    Update Word Search.cpp

--- Backtracking/Word Search.cpp
+++ Backtracking/Word Search.cpp
@@ -1,3 +1,10 @@
+// You are given a two-dimensional mat[][] of size n*m containing English alphabets and a string word. Check if the word exists on the mat. The word can be constructed by using letters from adjacent cells, either horizontally or vertically. The same cell cannot be used more than once.
+
+// Examples :
+
+// Input: mat[][] = [['T', 'E', 'E'], ['S', 'G', 'K'], ['T', 'E', 'L']], word = "GEEK"
+// Output: true
+
 class Solution {
   public:
     bool isSafe(int idx, int row, int col, int n, int m, vector<vector<char>>& mat, string& word, vector<vector<int>>& visit){
